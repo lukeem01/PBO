b920ff88104b362afa8615bf6c3bb216397e4f1a
lukeem01 <145565621+lukeem01@users.noreply.github.com>
2026-04-29 22:26:24 +0700

    Task

---
+++ PBO/sesi 6/Tugas/laptopUser.java
@@ -0,0 +1,23 @@
+public class laptopUser {
+    private Laptop laptop;
+
+    public laptopUser(Laptop laptop){
+        this.laptop = laptop;
+    }
+
+    void turnOnLaptop(){
+        this.laptop.powerOn();
+    }
+
+    void turnOffLaptop(){
+        this.laptop.powerOff();
+    }
+
+    void makeLaptopLouder(){
+        this.laptop.volumeUp();
+    }
+
+    void makeLaptopSilence(){
+        this.laptop.volumeDown();
+    }
+}
